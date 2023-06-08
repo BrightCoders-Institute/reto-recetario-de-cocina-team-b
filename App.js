6ac604d6a1bff448edb7ac97201377cf2e853b39
DerekAyala <derek.ayala12345@hotmail.com>
2023-06-07 19:21:10 -0600

    Implemented initial code structure for further development
    Co-authored-by: BrandonVG <brandon.vadillox3@gmail.com>
    Co-authored-by: Tole15 <rodrigermoon@gmail.com>
    Co-authored-by: FebernalGober <alexis.ferber18@hotmail.com>

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
+import{ NavigationContainer } from '@react-navigation/native' //importa la barra de navegación
+import { createStackNavigator} from '@react-navigation/stack'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Inicio" component={HomeScreen}/>
+          <Stack.Screen name="Detalle" component={DetailScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
