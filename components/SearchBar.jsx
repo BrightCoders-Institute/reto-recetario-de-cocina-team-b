b47355b7ef06671e761e242fd61072d04b128201
Tole15 <rodrigermoon@gmail.com>
2023-06-13 03:22:26 -0600

    Refactor SearchBar and HomeScreen components
    Refactored the SearchBar and HomeScreen components to improve code organization and readability. The changes include:
    
    SearchBar Component:
    - Reorganized import statements to group related modules together.
    - Added the useNavigation hook from React Navigation to access the navigation prop.
    - Modified the handleSearch function to filter the recipes based on the search term and also retrieve suggestions that start with the search term.
    - Added a new function, handleSuggestionPress, to handle suggestion selection, set the search term, update the filtered recipes, clear the suggestions, and navigate to the "Detalle" screen with relevant item and section information.
    - Renamed the onBlur prop of TextInput to handleBlur for better clarity.
    - Updated the JSX structure to separate the suggestions container from the main container, improving readability.
    - Modified the styling of the component, including background color, margin, padding, and alignment adjustments.
    
    HomeScreen Component:
    - Removed unused imports and unused variable declarations.
    - Removed the StatusBarManager import since it was not being used.
    - Adjusted the JSX structure to wrap the HorizontalLists in a separate container to maintain consistent spacing and layout.
    - Updated the styling of the component, including the removal of unnecessary margin and adjustment of the content container.
    
    These changes enhance the organization, maintainability, and user experience of the SearchBar and HomeScreen components.
    
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: FebernalGober <alexis.ferber18@gmail.com>
    Co-authored-by: BrandonVG <brandon.vadillox3@gmail.com>

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,49 +1,72 @@
-import React, { useState } from 'react';
-import { TextInput, StyleSheet, View, FlatList,TouchableOpacity,Text } from 'react-native';
-import Feather from 'react-native-vector-icons/Feather';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React, { useState } from "react";
+import { TextInput, StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Feather from "react-native-vector-icons/Feather";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const SearchBar = ({ recipes, setFilteredRecipes }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [suggestions,setSuggestions]= useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const navigation = useNavigation();
 
   const handleSearch = (text) => {
     setSearchTerm(text);
-    const filteredRecipes = recipes.filter(recipe =>
+
+    const filteredRecipes = recipes.filter((recipe) =>
       recipe.nombre.toLowerCase().includes(text.toLowerCase())
     );
+
     setFilteredRecipes(filteredRecipes);
+
+    const searchSuggestions = recipes.filter((recipe) =>
+      recipe.nombre.toLowerCase().startsWith(text.toLowerCase())
+    );
+
+    setSuggestions(searchSuggestions);
+  };
+
+  const handleSuggestionPress = (suggestion) => {
+    if (suggestion && suggestion.nombre) {
+      setSearchTerm(suggestion.nombre);
+      setFilteredRecipes([suggestion]);
+      setSuggestions([]);
+      navigation.navigate("Detalle", {
+        item: suggestion,
+        section: "suggestions",
+      });
+    }
+  };
+
+  const handleBlur = () => {
     setSuggestions([]);
   };
-const handleSuggestionPress = (suggestion)=>{
-  setSearchTerm(suggestion.nombre);
-  setSuggestions([]);
-  setFilteredRecipes([suggestion]);
-};
+
   return (
-    <View style={[styles.row,styles.barra]}>
-      <Feather style={styles.icon} name="search" size={20} color="#f5fdff"/>
-      <TextInput
-        style= {styles.input}
-        placeholder="What do you want to eat? "
-        placeholderTextColor={ '#f5fdff' }
-        value={searchTerm}
-        onChangeText={handleSearch}
-        onBlur={()=> setSuggestions([])}
-      />
-      <FontAwesome style={styles.icon} name="microphone" size={20} color="#ffffff"/> 
-      {suggestions.length > 0 && (
-        <View>
-          <FlatList
-            data={suggestions}
-            renderItem={ ( {item}) => (
-              <TouchableOpacity
-              onPress={()=>handleSuggestionPress(item)}>
-              <Text>{item.nombre}</Text>
+    <View style={styles.container}>
+      <View style={[styles.row, styles.barra]}>
+        <Feather style={styles.icon} name="search" size={20} color="#f5fdff" />
+        <TextInput
+          style={styles.input}
+          placeholder="What do you want to eat? "
+          placeholderTextColor="#f5fdff"
+          value={searchTerm}
+          onChangeText={handleSearch}
+          onBlur={handleBlur}
+        />
+        <FontAwesome style={styles.icon} name="microphone" size={20} color="#ffffff" />
+      </View>
+      {searchTerm.length > 0 && (
+        <View style={styles.suggestionsContainer}>
+          {suggestions.map((item) => (
+            <TouchableOpacity
+              key={item.id}
+              onPress={() => handleSuggestionPress(item)}
+              style={styles.suggestionItem}
+            >
+              <Text style={styles.suggestionText}>{item.nombre}</Text>
             </TouchableOpacity>
-            )}
-          keyExtractor={(item)=> item.id.toString()}
-          />
+          ))}
         </View>
       )}
     </View>
@@ -51,24 +74,49 @@ const handleSuggestionPress = (suggestion)=>{
 };
 
 const styles = StyleSheet.create({
-  row:{
-    flexDirection:'row',
+  container: {
+    marginTop: 0,
+  },
+  row: {
+    flexDirection: "row",
   },
   barra: {
-    backgroundColor: '#343435',
+    backgroundColor: "#343435",
     marginHorizontal: 10,
     borderRadius: 10,
-    paddingVertical: 5, 
+    paddingVertical: 5,
     paddingHorizontal: 7,
     marginBottom: 30,
     marginTop: 10,
-    alignItems: 'center',
-    justifyContent:'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  input:{
-    flex:1,
+  input: {
+    flex: 1,
     paddingLeft: 5,
-  }
-})
+    color: "#f5fdff",
+  },
+  suggestionsContainer: {
+    flexDirection: "column",
+    marginLeft: 10,
+    marginBottom: 10,
+    marginTop: -10,
+    backgroundColor: "#343435",
+    borderRadius: 10,
+    maxHeight: 100,
+    paddingHorizontal: 7,
+    paddingVertical: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    maxWidth: 370,
+  },
+  suggestionItem: {
+    paddingVertical: 5,
+  },
+  suggestionText: {
+    fontSize: 16,
+    color: "#f5fdff",
+  },
+});
 
 export default SearchBar;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,32 +8,30 @@ import HorizontalList from '../components/horizontalList';
 
 const data = require('../Recetas.json')
 
-export default function HomeScreen({navigation}){
+export default function HomeScreen({ navigation }) {
+  const [filteredRecipes, setFilteredRecipes] = useState(data.recetas);
 
-  const [filteredRecipes, setFilteredRecipes] = useState(data.recetas); // Estado para almacenar las recetas filtradas
-  
-  
-  
-  return(
+  return (
     <SafeAreaView style={styles.container}>
       <StatusBar style='auto'/>
       <SearchBar recipes={data.recetas} setFilteredRecipes={setFilteredRecipes} /> 
-      <FlatList
-        data={filteredRecipes}
-        renderItem={({item})=><ListItem recipe={item}/>}
-        keyExtractor={(item)=>item.id.toString()}
-        ListEmptyComponent={<Text>No existe mi bro lo siento</Text>}
-      />
-      <HorizontalList data={data.recetas} titulo={"TRENDING"} imgHeight={115} imgWidth={115} />
-      <HorizontalList data={data.recetas} titulo={"RECENT"} imgHeight={190} imgWidth={170}  />
+      <View style={styles.contentContainer}>
+        <HorizontalList data={data.recetas} titulo={"TRENDING"} imgHeight={115} imgWidth={115} />
+        <HorizontalList data={data.recetas} titulo={"RECENT"} imgHeight={190} imgWidth={170}  />
+      </View>
     </SafeAreaView>
-  ); 
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#282828',
     paddingTop: Platform.OS === 'android' ? StatusBarManager.HEIGHT : 0,
   },
-
+  contentContainer: {
+    flex: 1,
+    marginBottom: 0, // Asegurarse de que marginBottom sea 0
+  },
 });
+
